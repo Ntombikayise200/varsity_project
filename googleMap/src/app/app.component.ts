d4b5f0b6ff25b0e0a40caabbb7197289a6c7fa06
Academy_learner_DAmac10 <academy_learner_damac10@Academy-learner-DAmac10s-MacBook-Air.local>
2021-06-10 16:58:44 +0200

    changes

--- googleMap/src/app/app.component.ts
+++ googleMap/src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'googleMap';
+
+  lat = 22.4064172;
+  long = 69.0750171;
+  zoom=7;
+  
+  markers = [
+        {
+            lat: 21.1594627,
+            lng: 72.6822083,
+            label: 'Surat'
+        },
+        {
+            lat: 23.0204978,
+            lng: 72.4396548,
+            label: 'Ahmedabad'
+        },
+        {
+            lat: 22.2736308,
+            lng: 70.7512555,
+            label: 'Rajkot'
+        }
+    ];
 }
--- googleMap/src/app/app.module.ts
+++ googleMap/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocationComponent } from './components/location/location.component';
 
+import { AgmCoreModule } from '@agm/core';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey:'AIzaSyC6CWqBSRI5GILTJr0G3SFnYM0fAZMFu7o'
+      // libraries:['Johannesburg']
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
